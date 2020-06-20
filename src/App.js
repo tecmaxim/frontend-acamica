3a6d6ed654591267b8b083d45664e02868482776
Maximiliano Mendoza <mmendoza@aivo.co>
2020-06-20 17:45:43 -0300

    auto formater runed on app.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
@@ -32,17 +31,13 @@ function App() {
             <Link class="btn btn-info " to="/form">New Student</Link>
           </div>
         </nav>
-
         <div className="container py-4">
           <div className="row">
-
             <Route path="/" exact component={List} />
             <Route path="/form" component={Form} />
             <Route path="/edit" component={Edit} />
-
           </div>
         </div>
-
       </div>
     </Router>
   );
