49d2b31a4488fe96fa83d893d659e1252e1e5d36
Maximiliano Mendoza <mmendoza@aivo.co>
2020-06-20 17:43:31 -0300

    HTML Router and containers added

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# React Frontend Acamica
+# Challenge React Frontend Acamica
+### v1.0.0
+
+## How to start
+### Clone repository
+```bash
+git clone https://github.com/tecmaxim/api-acamica.git
+```
+### Install dependencies, copy env file set the main settings and run the main script
+```bash
+cd api-acamica/
+npm i
+# You must config variables as you need. example your db host:user:pw
+cp .env.example .env
+node app.js
+```
+### IMPORTANT
+It is mandatory to have the Main API running for this to work properly:
+https://github.com/tecmaxim/node-api-acamica.git
+
+## Run with Docker
+
+1) Install Docker.
+
+2) Create .env file using .env.example info (Modify envs vars as much as you need).
+
+3) In project root execute ```~$ docker build -t [docker-repository-name] .```.
+
+4) Then run the docker ```~$ sudo docker run --publish [port]:[port] --name [docker-image-name]  -v [absolute root to the proyect]:/[proyect-name] -d [docker-repository-name]```.
+
+5) Try to get into the bash with ```~$ docker exec -it [docker-image-name] bash```.
--- src/App.js
+++ src/App.js
@@ -3,19 +3,48 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// Import modules 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// Import modules
 import Form from './modules/Add';
 import List from './modules/List';
 import Edit from './modules/Edit';
 
 /**
- *  
+ *
  * Render de MAIN view whit student list
- * 
+ *
  */
 function App() {
+  return (
+    <Router>
+      <div className="App">
 
-  // TODO
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <Link class="nav-link" to="/"> Student List </Link>
+              </li>
+            </ul>
+            <Link class="btn btn-info " to="/form">New Student</Link>
+          </div>
+        </nav>
+
+        <div className="container py-4">
+          <div className="row">
+
+            <Route path="/" exact component={List} />
+            <Route path="/form" component={Form} />
+            <Route path="/edit" component={Edit} />
+
+          </div>
+        </div>
+
+      </div>
+    </Router>
+  );
 }
 export default App;
